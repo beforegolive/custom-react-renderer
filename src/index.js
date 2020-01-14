bab6672b0cd6600f3134a956beffc4dddb3d8c55
twomeetings <2326220@163.com>
2020-01-15 06:00:27 +0800

    step 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Text = props => {
+    return <p className={props.className} > {props.content}</p>
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App =()=>{
+  return (
+    <div>
+      <Text className="hello-class" content="Hello" />
+      <span style={{color:'blue'}}>World</span>
+    </div>      
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
